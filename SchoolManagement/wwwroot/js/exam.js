314a93a7ba5a6812b1fcd03d6a4b38820120cfba
SashoLichovski <licovskiph@gmail.com>
2020-07-21 13:15:40 +0200

    setTime function

--- SchoolManagement/wwwroot/js/exam.js
+++ SchoolManagement/wwwroot/js/exam.js
@@ -1,1 +1,6 @@
-﻿
+﻿function setTime() {
+    var startDate = document.getElementById("startDate").value;
+    var endDate = document.getElementById("endDate");
+    endDate.value = startDate;
+    //endDate.value.setHours(endDate.value.getHours() + 2);
+}
